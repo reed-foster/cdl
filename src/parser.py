3321d50fa7b53d1882efb6c5c42412da2afbd972
reed_foster <reed.a.foster@gmail.com>
2017-11-16 11:20:45 -0800

    moved ast classes to separate file

--- src/parser.py
+++ src/parser.py
@@ -1,35 +1,3 @@
-class AST():
-    pass
-
-class TernaryOp(AST):
-    def __init__(self, boolean, op, left, right):
-        self.boolean = boolean
-        self.token = self.op = op
-        self.left = left
-        self.right = right
-
-class BinaryOp(AST):
-    def __init__(self, left, op, right):
-        self.left = left
-        self.token = self.op = op
-        self.right = right
-
-class UnaryOp(AST):
-    def __init__(self, op, right):
-        self.token = self.op = op
-        self.right = right
-
-class Assign(AST):
-    def __init__(self, left, op, right):
-        self.left = left
-        self.token = self.op = op
-        self.right = right
-
-class Var(AST):
-    def __init__(self, token):
-        self.token = token
-        self.value = token.value
-
 class Parser():
 
     def __init__(self, lexer):
