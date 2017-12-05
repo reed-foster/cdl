8eb687b3398a2c33dece58f5e67558c7086285a5
reed_foster <reed.a.foster@gmail.com>
2017-12-05 11:00:16 -0800

    working on implementation of parser. still need to finish architecture parsing and have not started expression parsing

--- src/parser.py
+++ src/parser.py
@@ -3,6 +3,8 @@ class Parser():
     def __init__(self, lexer):
         self.lexer = lexer
         self.current_token = self.lexer.getNextToken()
+        varlist = {}
+        siglist = {}
 
     def error(self):
         raise Exception('Syntax Error')
@@ -72,3 +74,104 @@ class Parser():
             node = UnaryOp(token, self.relationalexpression())
             return node
         #if self.current_token.type
+    
+    def gendeclare(self):
+        self.eat(TYPE)
+        token = self.current_token
+        
+
+    def sigdeclare(self):
+        self.eat(SIGNAL)
+        self.eat(TYPE)
+        token = self.current_token
+
+
+    def vardeclare(self):
+
+    def sigassign(self):
+        left = self.signal()
+        token = self.current_token
+        self.eat(SIGASSIGN)
+
+    def component(self):
+        self.eat('COMPONENT')
+        name = self.current_token
+        self.eat(ID)
+        self.eat(LBRACE)
+        body = ComponentBody()
+        while self.current_token.type != RBRACE:
+            if self.current_token.type == TYPE:
+                body.children.append(self.generic())
+            elif self.current_token.type == 'PORT':
+                # TODO: add error for multiple port declarations
+                body.children.append(self.port())
+            elif self.current_token.type == ID || self.current_token.type == 'ARCH':
+                body.children.append(self.arch())
+        self.eat(RBRACE)
+        node = Component(name, body)
+        return node
+
+    def generic(self):
+        type = self.current_token
+        self.eat(TYPE)
+        node = Generic(self.current_token, type)
+        self.eat(ID)
+        self.eat(EOL)
+        return node
+
+    def port(self):
+        self.eat('PORT')
+        self.eat(LBRACE)
+        body = PortList()
+        while self.current_token.type != RBRACE:
+            direction = self.current_token
+            self.eat(PORTDIR)
+            type = self.current_token
+            self.eat(TYPE)
+            body.children.append(Port(self.current_token, type, direction))
+            self.eat(ID)
+            self.eat(EOL)
+        self.eat(RBRACE)
+        return body
+
+    def arch(self):
+        name = None
+        if self.current_token == ID:
+            name = self.current_token
+            self.eat(ID)
+        self.eat('ARCH')
+        self.eat(LBRACE)
+        body = ArchBody()
+        while self.current_token.type != RBRACE:
+            if self.current_token.type == SIGNAL:
+                body.children.append(self.signaldeclaration())
+            elif self.current_token.type == ID:
+                #need to distinguish b/w component instatiation and signal assignment
+                token = self.current_token
+                self.eat(ID)
+                if self.current_token.type == SIGASSIGN:
+                    self.eat(SIGASSIGN)
+                    body.children.append(Assign(token, self.expression()))
+                elif self.current_token.type == ID:
+                    name = self.current_token
+                    self.eat(ID)
+                    self.eat(COMPASSIGN)
+                    self.eat('NEW')
+                    if self.current_token.type == ID and self.current_token.value == token.value:
+                        self.eat(ID)
+                        self.eat(LPAREN)
+                        #parse generic assignment list
+                        generics = 
+                        self.eat(RPAREN)
+                        body.children.append(CompInst(name, generics))
+                    else:
+                        self.error()
+            elif self.current_token.type == 'CONNECT':
+                body.children.append(self.connect())
+            elif self.current_token.type == 'PROCESS':
+                body.children.append(self.process())
+            elif self.current_token.type == 'GENERATE':
+                body.children.append(self.generate())
+        self.eat(RBRACE)
+        node = Arch(name, body)
+        return node
