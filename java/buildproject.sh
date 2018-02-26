4e34b527bfc8d52d4254b1630dcd3e8cdd2a50c2
reed_foster <reed.a.foster@gmail.com>
2018-02-26 11:50:34 -0800

    added automation of building java project

---
+++ java/buildproject.sh
@@ -0,0 +1,10 @@
+# Utility for building java project
+# Supply class name as an argument to execute that class
+
+javac -d ~/git/cdl/java/bin ~/git/cdl/java/src/com/foster/cdl/*.java
+
+if ! [ -z $1 ]
+then
+  filename="com.foster.cdl.$1"
+  java -cp ~/git/cdl/java/bin $filename
+fi
