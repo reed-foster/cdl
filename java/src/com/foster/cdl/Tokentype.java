fd481b566bd0dc16d73340c872b66c5e890eb865
reed_foster <reed.a.foster@gmail.com>
2018-03-02 15:26:44 -0800

    added reserved identifier token type

--- java/src/com/foster/cdl/Tokentype.java
+++ java/src/com/foster/cdl/Tokentype.java
@@ -1,8 +1,13 @@
+/*
+Tokentype.java - Reed Foster
+Enum for Token types
+*/
+
 package com.foster.cdl;
 
 public enum Tokentype
 {
-    ID, DECINTCONST, BININTCONST, HEXINTCONST, BINVECCONST, HEXVECCONST, BOOLCONST, // identifier and literals
+    ID, RESERVED, DECINTCONST, BININTCONST, HEXINTCONST, BINVECCONST, HEXVECCONST, BOOLCONST, // identifier and literals
     COLEQ, EQ, DOUBLEEQ, LT, GT, LTEQ, GTEQ, NE, // assignment and comparison operators
     ADD, SUB, MUL, DIV, MOD, EXP, AND, OR, XOR, NOT, COLON, QUESTION, // operators
     LPAREN, RPAREN, LBRACE, RBRACE, LBRACKET, RBRACKET, // grouping
