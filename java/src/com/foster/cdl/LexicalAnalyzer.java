768cb5fe34b9707c0626209c550bc7d2ba959f22
reed_foster <reed.a.foster@gmail.com>
2018-03-05 15:20:23 -0800

    added new syntax error message

--- java/src/com/foster/cdl/LexicalAnalyzer.java
+++ java/src/com/foster/cdl/LexicalAnalyzer.java
@@ -11,4 +11,9 @@ public class LexicalAnalyzer
     {
         throw new SyntaxError(String.format("%s (%s) on line %d.", message, token, line));
     }
+
+    public static void error(String message, int line) throws SyntaxError
+    {
+        throw new SyntaxError(String.format("%s on line %d.", message, line));
+    }
 }
