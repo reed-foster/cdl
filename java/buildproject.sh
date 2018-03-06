66a351c75b372d759f0f37cde46d1d5cb35a38c4
reed_foster <reed.a.foster@gmail.com>
2018-03-06 10:04:23 -0800

    added -Xlint:unchecked flag

--- java/buildproject.sh
+++ java/buildproject.sh
@@ -1,7 +1,7 @@
 # Utility for building java project
 # Supply class name as an argument to execute that class
 
-javac -d ~/git/cdl/java/bin ~/git/cdl/java/src/com/foster/cdl/*.java
+javac -d ~/git/cdl/java/bin ~/git/cdl/java/src/com/foster/cdl/*.java -Xlint:unchecked
 
 if ! [ -z $1 ]
 then
