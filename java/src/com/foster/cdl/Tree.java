8587795dbfd8bba71bff9f650908547c31e1bd5c
reed-foster <reed.a.foster@gmail.com>
2018-04-04 17:42:40 -0700

    added documentation for each method, also made visit method private

--- java/src/com/foster/cdl/Tree.java
+++ java/src/com/foster/cdl/Tree.java
@@ -13,6 +13,9 @@ public class Tree
     public final Map<String, String> attributes;
     private List<Tree> children;
     
+    /**
+    * Default Tree constructor
+    */
     Tree(Nodetype nodetype, Map<String, String> attributes, List<Tree> children)
     {
         this.nodetype = nodetype;
@@ -20,47 +23,76 @@ public class Tree
         this.children = children;
     }
 
+    /**
+    * Tree constructor for nodes without attributes (like port declarations)
+    */
     Tree(Nodetype nodetype, List<Tree> children)
     {
         this(nodetype, new HashMap<String, String>(), children);
     }
 
+    /**
+    * Tree constructor for nodes with no children (like identifiers)
+    */
     Tree(Nodetype nodetype, Map<String, String> attributes)
     {
         this(nodetype, attributes, new ArrayList<Tree>());
     }
 
+    /**
+    * Modifier method for adding children to the tree
+    * @param child subtree to be added
+    */
     public void addChild(Tree child)
     {
         this.children.add(child);
     }
 
+    /**
+    * Modifier method for removing children from the tree
+    * @param index int which child to remove (children are stored in a list so insertion order is preserved)
+    */
     public void removeChild(int index)
     {
         this.children.remove(index);
     }
 
+    /**
+    * Get the size of the child list
+    */
     public int numChildren()
     {
         return this.children.size();
     }
     
+    /**
+    * Returns the child at the specified index (children are stored in a list so insertion order is preserved)
+    */
     public Tree getChild(int index)
     {
         return children.get(index);
     }
 
+    /**
+    * Returns a new ArrayList containing all the children
+    */
     public List<Tree> getChildren()
     {
         return new ArrayList<Tree>(this.children);
     }
 
+    /**
+    * Returns a string representation of the tree, with each generation of children indented 2 spaces
+    */
     public String toString()
     {
         return this.visit(0);
     }
 
-    public String visit(int depth)
+    /**
+    * Helper method for toString()
+    */
+    private String visit(int depth)
     {
         String s = String.format("%s : %s.\n", this.nodetype.toString(), this.attributes.toString());
         for (Tree child : this.children)
