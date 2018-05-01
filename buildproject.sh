a1d26a083fbbd743a2a2912990ef3ed0578d5f1d
reed_foster <reed.a.foster@gmail.com>
2018-05-01 11:20:13 -0700

    forgot to change pathnames in buildscript when moving files around

--- buildproject.sh
+++ buildproject.sh
@@ -1,10 +1,10 @@
 # Utility for building java project
 # Supply class name as an argument to execute that class
 
-javac -d ~/git/cdl/java/bin ~/git/cdl/java/src/com/foster/cdl/*.java -Xlint:unchecked
+javac -d ~/git/cdl/bin ~/git/cdl/src/com/foster/cdl/*.java -Xlint:unchecked
 
 if ! [ -z $1 ]
 then
   filename="com.foster.cdl.$1"
-  java -cp ~/git/cdl/java/bin $filename
+  java -cp ~/git/cdl/bin $filename
 fi
