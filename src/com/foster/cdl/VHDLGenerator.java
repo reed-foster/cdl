6457a1b23a22a026b04bc8b879e14a3e45208145
reed_foster <reed.a.foster@gmail.com>
2018-05-01 11:23:50 -0700

    works with singular components, need to debug cyclicity checking

--- src/com/foster/cdl/VHDLGenerator.java
+++ src/com/foster/cdl/VHDLGenerator.java
@@ -10,35 +10,57 @@ import java.util.*;
 public class VHDLGenerator
 {
     private Map<String, Component> components;
+    private Map<String, String> componentInterfaces;
     private Map<String, Set<DeclaredIdentifier>> tempSignals; // maps each component name to a set of declaredIdentifiers
+    private Map<String, Set<String>> usedPorts; // maps each component instance to a set of all ports that are used
 
     private String currentComponent;
-
-    public final output;
 
     VHDLGenerator(String source)
     {
         SemanticAnalyzer s = new SemanticAnalyzer(source);
         this.components = s.getComponents();
+        this.componentInterfaces = new HashMap<String, String>();
+        this.tempSignals = new HashMap<String, Set<DeclaredIdentifier>>();
+        this.usedPorts = new HashMap<String, Set<String>>();
         this.getAllTempSignals();
-        this.output = "";
-        for (String compname : this.components.keys())
+    }
+
+    public String getVHDL()
+    {
+        for (String compname : this.components.keySet()) // first, generate all the entity declarations
+        {
+            Tree ast = this.components.get(compname).ast;
+            this.componentInterfaces.put(compname, this.getEntity(ast));
+        }
+        String output = "";
+        for (String compname : this.components.keySet())
         {
             this.currentComponent = compname;
-            output += this.visit(this.components.get(compname));
+            Tree ast = this.components.get(compname).ast;
+            output += this.componentInterfaces.get(compname) + "\n\n" + this.getArch(ast);
         }
-
+        return output;
     }
 
     private void getAllTempSignals()
     {
-        for (String componentName : this.components.keys())
+        for (String componentName : this.components.keySet())
         {
             Component component = this.components.get(componentName);
-            this.tempSignals.put(componentName, this.getTempSignals(componentName, component.ast));
+            Set<DeclaredIdentifier> tempSignals = this.getTempSignals(componentName, component.ast);
+            this.tempSignals.put(componentName, tempSignals);
+            Set<String> usedPorts = new HashSet<String>();
+            for (DeclaredIdentifier tempSignal : tempSignals)
+                usedPorts.add(tempSignal.name);
+            this.usedPorts.put(componentName, usedPorts);
         }
     }
 
+    /**
+    * Creates a Set of DeclaredIdentifiers for all subcomponent ports that are used as signals
+    *
+    */
     private Set<DeclaredIdentifier> getTempSignals(String componentName, Tree node)
     {
         Set<DeclaredIdentifier> sigDecs = new HashSet<DeclaredIdentifier>();
@@ -47,10 +69,10 @@ public class VHDLGenerator
             List<Tree> children = new ArrayList<Tree>();
             Map<String, String> attributes = new HashMap<String, String>();
 
-            String compinstID = node.getChlid(0).attributes.get("name");
-            String portID = node.getChlid(1).attributes.get("name");
+            String compinstID = node.getChild(0).attributes.get("name");
+            String portID = node.getChild(1).attributes.get("name");
             attributes.put("compname", compinstID);
-            attributes.put("portname", portID);
+            attributes.put("name", portID);
 
             for (DeclaredIdentifier subcomp : this.components.get(componentName).getSubcomponents())
             {
@@ -73,7 +95,7 @@ public class VHDLGenerator
         }
         else
         {
-            for (Tree child : node.getChlidren())
+            for (Tree child : node.getChildren())
             {
                 sigDecs.addAll(this.getTempSignals(componentName, child));
             }
@@ -81,83 +103,131 @@ public class VHDLGenerator
         return sigDecs;
     }
 
+    private String getEntity(Tree node)
+    {
+        if (node.nodetype != Nodetype.COMPONENT)
+            return null;
+        String generics = "";
+        String ports = "";
+        for (Tree child : node.getChildren())
+        {
+            switch (child.nodetype)
+            {
+                case GENDEC:
+                    generics += node.attributes.get("name") + " : " + this.getType(node) + ";\n";
+                    break;
+                case PORTDEC:
+                    String body = "";
+                    for (Tree grandchild : child.getChildren())
+                    {
+                        String direction = grandchild.attributes.get("direction");
+                        direction = direction.substring(0, direction.length() - 3); // input => in, output => out
+                        body += grandchild.attributes.get("name") + " : " + direction + " " + this.getType(grandchild) + ";\n";
+                    }
+                    if (body.length() != 0)
+                        ports = indent("port\n(\n" + indent(body.substring(0, body.length() - 2)) + "\n);") + "\n"; // substring to remove last trailing semicolon
+                    break;
+                }
+        }
+        String entity = "entity " + node.attributes.get("name") + " is\n";
+        entity += generics.length() > 1 ? indent("generic\n(\n" + generics + "\n);") + "\n" : "";
+        entity += ports;
+        entity += "end " + node.attributes.get("name") + ";";
+        return entity;
+    }
+
+    private String getArch(Tree node)
+    {
+        String arch = "";
+        if (node.nodetype == Nodetype.ARCH)
+            arch += this.visit(node);
+        else if (node.nodetype == Nodetype.COMPONENT)
+        {
+            for (Tree child : node.getChildren())
+                arch += this.getArch(child);
+        }
+        return arch;
+    }
+
+    // should only be called on Arch (need to refactor names)
     private String visit(Tree node)
     {
         switch (node.nodetype)
         {
-            case COMPONENT:
-                String header = "library ieee;\nuse ieee.std_logic_1164.all;\nuse ieee.numeric_std.all;";
-                String generics = "";
-                String ports = "";
-                String arch = "";
-                for (Tree child : node.getChlidren())
-                {
-                    switch (child.nodetype)
-                    {
-                        case GENDEC:
-                            generics += this.visit(child);
-                            break;
-                        case PORTDEC:
-                            ports += this.visit(child);
-                            break;
-                        case ARCH:
-                            arch += this.visit(child);
-                            break;
-                    }
-                }
-                String body = header + "entity " + node.attributes.get("name") + "\n";
-                body += generics.length() > 1 ? "generic\n(\n" + generics + "\n);" : "";
-                body += ports + "\nend entity";
-                body += arch;
-                return body;
-            case GENDEC:
-                return node.attributes.get("name") + " : " + this.getType(node);
-            case PORTDEC:
-                String body = ""
-                for (Tree child : node.getChlidren())
-                {
-                    body += this.visit(child) + ";\n";
-                }
-                if (body.length() == 0)
-                    return "port();";
-                return "port\n(\n" + body.substring(0, body.length() - 2) + ");"; // substring to remove last trailing semicolon
-            case PORT:
-                String direction = node.attributes.get("direction");
-                direction = direction.substring(0, direction.length() - 3); // input => in, output => out
-                return node.attributes.get("name") + " : " + direction + " " + this.getType(node);
             case ARCH:
-                String body = "architecture structural of " + this.currentComponent + "is";
-                String delcarations = "";
+                String declarations = "";
                 String assignments = "";
-                for (Tree child : node.getChlidren())
+                for (Tree child : node.getChildren())
                 {
                     switch (child.nodetype)
                     {
                         case COMPDEC:
+                            // add component declaration to declarations
+                            String interfaceName = child.attributes.get("type");
+                            String componentInterface = this.componentInterfaces.get(interfaceName);
+                            int firstnewline = componentInterface.indexOf('\n');
+                            int lastnewline = componentInterface.lastIndexOf('\n');
+                            declarations += "component " + interfaceName + "\n" + componentInterface.substring(firstnewline + 1, lastnewline) + "\nend component;\n";
+                            
+                            // add component instantiation to assignments
+                            String instanceName = child.attributes.get("name");
+                            Set<DeclaredIdentifier> ports = this.components.get(interfaceName).getPorts();
+                            Set<DeclaredIdentifier> generics = this.components.get(interfaceName).getGenerics();
+                            String portMap = "";
+                            for (DeclaredIdentifier port : ports)
+                            {
+                                portMap += port.name + " => ";
+                                portMap += this.usedPorts.get(instanceName).contains(port.name) ? instanceName + "_" + port.name : "open";
+                                portMap += ",\n";
+                            }
+                            String genericMap = "";
+                            for (Tree genericAssign : child.getChildren())
+                            {
+                                genericMap += this.visit(genericAssign.getChild(0)) + " => " + this.visit(genericAssign.getChild(1)) + ",\n";
+                            }
+                            portMap = portMap.length() > 0 ? "port map\n(\n" + portMap.substring(0, portMap.length() - 2) + "\n)\n" : "";
+                            genericMap = genericMap.length() > 0 ? "generic map\n(\n" + genericMap.substring(0, genericMap.length() - 2) + "\n)\n" : "";
+                            assignments += instanceName + " : " + interfaceName + "\n" + genericMap + portMap + ";\n";
+                            break;
                         case SIGDEC:
                         case CONST:
+                            declarations += this.visit(child) + ";\n";
+                            break;
                         case BINARYOP:
+                            assignments += this.visit(child) + ";\n";
+                            break;
+                        default:
+                            // shouldn't ever get here
+                            break;
                     }
-                    declarations += this.visit(child) + ";\n";
                 }
-            case COMPDEC:
+                for (DeclaredIdentifier tempSignal : this.tempSignals.get(this.currentComponent))
+                {
+                    declarations += "signal " + tempSignal.declaration.attributes.get("compname") + "_" + tempSignal.name + " : " + this.getType(tempSignal.declaration);
+                }
+                return "architecture structural of " + this.currentComponent + " is\n" + indent(declarations) + "\nbegin\n" + indent(assignments) + "\nend structural;";
             case SIGDEC:
+                return "signal " + node.attributes.get("name") + " : " + this.getType(node);
             case CONST:
+                return "constant " + node.attributes.get("name") + " : " + this.getType(node) + " := " + this.visit(node.getChild(0));
             case TERNARYOP:
                 if (node.attributes.get("type").equals("?"))
                     return this.visit(node.getChild(1)) + " when " + this.visit(node.getChild(0)) + " else " + this.visit(node.getChild(2));
                 else if (node.attributes.get("type").equals("[]"))
                 {
-                    String upper = this.visit(node.getChild(1))
+                    String upper = this.visit(node.getChild(1));
                     String lower = node.numChildren() == 2 ? upper : this.visit(node.getChild(2));
                     return this.visit(node.getChild(0)) + "(" + upper + " downto " + lower + ")";
                 }
                 break;
             case BINARYOP:
-                return this.visit(node.getChild(0)) + " " + node.attributes.get("type") + " " + this.visit(node.getChild(1));
+                String sep = node.attributes.get("type");
+                if (sep.equals("."))
+                    sep = "_";
+                return this.visit(node.getChild(0)) + " " + sep + " " + this.visit(node.getChild(1));
             case UNARYOP:
                 if (node.attributes.get("type").equals("()"))
-                    return "(" + this.vist(node.getChild(0)) + ")";
+                    return "(" + this.visit(node.getChild(0)) + ")";
                 else if (node.attributes.get("type").equals("not"))
                     return "not " + this.visit(node.getChild(0));
                 else
@@ -167,15 +237,22 @@ public class VHDLGenerator
             case LITERAL:
                 return node.attributes.get("value");
         }
+        return "";
     }
 
-    private String indent(String str, int spaces)
+    private static String indent(String str)
     {
+        return indent(str, 4);
+    }
+
+    private static String indent(String str, int spaces)
+    {
+        String indentStr = new String(new char[spaces]).replace("\0", " ");
         String[] split = str.split("\n");
         String result = "";
         for (String element : split)
-            result = result + String.format("%1$" + spaces + "s", element) + "\n";
-        return result;
+            result += indentStr + element + "\n";
+        return result.substring(0, result.length() - 1);
     }
 
     private String getType(Tree node)
@@ -190,7 +267,17 @@ public class VHDLGenerator
                 type = "boolean";
                 break;
             case "vec":
-                type = "unsigned(" + this.visit(node.getChild(0)) + " downto 0)";
+                String width = this.visit(node.getChild(0));
+                String newWidth;
+                try
+                {
+                    newWidth = new Integer(Integer.valueOf(width) - 1).toString();
+                }
+                catch (NumberFormatException e)
+                {
+                    newWidth = "(" + width + " - 1)";
+                }
+                type = "unsigned(" + newWidth + (newWidth.equals("0") ? ")" : " downto 0)");
                 break;
             default:
                 type = "";
@@ -201,6 +288,47 @@ public class VHDLGenerator
 
     public static void main(String[] args)
     {
+        String source = "component XorGate{"+
+                            "port{"+
+                                "input vec[1] a;"+
+                                "input vec[1] b;"+
+                                "output vec[1] sum;"+
+                            "}"+
+                            "arch{"+
+                                "sum <= a xor b;"+
+                            "}"+
+                        "}"+
+                        "component AndGate{"+
+                            "port{"+
+                                "input vec[1] a;"+
+                                "input vec[1] b;"+
+                                "output vec[1] prod;"+
+                            "}"+
+                            "arch{"+
+                                "prod <= a and b;"+
+                            "}"+
+                        "}"+
+                        "component HalfAdder{"+
+                            "port{"+
+                                "input vec[1] a;"+
+                                "input vec[1] b;"+
+                                "input vec[1] ci;"+
+                                "output vec[1] sum;"+
+                                "output vec[1] co;"+
+                            "}"+
+                            "arch{"+
+                                "XorGate XOR = new XorGate();"+
+                                "AndGate AND = new AndGate();"+
+                                "XOR.a <= a;"+
+                                "XOR.b <= b;"+
+                                "AND.a <= a;"+
+                                "AND.b <= b;"+
+                                "sum <= XOR.sum;"+
+                                "co <= AND.prod;"+
+                            "}"+
+                        "}";
 
+        VHDLGenerator gen = new VHDLGenerator(source);
+        System.out.println(gen.getVHDL());
     }
 }
