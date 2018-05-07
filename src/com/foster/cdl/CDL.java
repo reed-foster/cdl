26e91ab165f9443db7dfc48c4d12bed3bb8985d8
reed_foster <reed.a.foster@gmail.com>
2018-05-06 20:37:40 -0700

    now writes to output file instead of printing to stdout

--- src/com/foster/cdl/CDL.java
+++ src/com/foster/cdl/CDL.java
@@ -5,6 +5,7 @@ Main class, reads source from files and writes to terminal for output
 
 package com.foster.cdl;
 
+import java.util.*;
 import java.nio.file.*;
 import java.nio.charset.Charset;
 import java.io.*;
@@ -17,19 +18,35 @@ public class CDL
         return new String(encoded, Charset.defaultCharset());
     }
 
+    private static void writeFile(String filename, String contents) throws IOException
+    {
+        List<String> lines = Arrays.asList(contents.split("\n"));
+        Path file = Paths.get(filename);
+        Files.write(file, lines, Charset.defaultCharset());//, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
     public static void main(String[] args)
     {
         if (args.length < 1)
+        {
+            System.out.println("Please supply a destination");
+            return;
+        }
+        String[] sourceFiles = new String[args.length - 1];
+        String dest = args[0];
+        for (int i = 1; i < args.length; i++)
+            sourceFiles[i - 1] = args[i];
+        if (sourceFiles.length < 1)
         {
             System.out.println("Please supply at least one source");
             return;
         }
         String source = "";
-        for (String arg : args)
+        for (String sourceFile : sourceFiles)
         {
             try
             {
-                source += readFile(arg) + "\n";
+                source += readFile(sourceFile) + "\n";
             }
             catch (IOException e)
             {
@@ -38,6 +55,15 @@ public class CDL
             }
         }
         VHDLGenerator gen = new VHDLGenerator(source);
-        System.out.println(gen.getVHDL());
+        String output = gen.getVHDL();
+        try
+        {
+            writeFile(dest, output);
+        }
+        catch (IOException e)
+        {
+            System.out.println("Failed to write to output file");
+            return;
+        }
     }
 }
